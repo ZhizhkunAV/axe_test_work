49727a9f0787e0c45ff853e59371e82f9d05be90
Andrey_Zhizhkun <zizkun.av@list.ru>
2024-05-10 13:22:57 +0400

    Message

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -3,7 +3,6 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
--- src/test/java/pages/SettingsPage.java
+++ src/test/java/pages/SettingsPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class SettingsPage {
 
