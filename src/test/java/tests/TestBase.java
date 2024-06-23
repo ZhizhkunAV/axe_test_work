faac70d57ee5414ca849d946bc11c8fd405387f7
Andrey <zizkun.av@list.ru>
2024-06-24 00:40:50 +0400

    Commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
-
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,29 +13,36 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 public class TestBase {
-    @BeforeAll
-    static void beforeAll() {
+
+        @BeforeAll
+        static void beforeAll () {
+            Configuration.baseUrl = "https://www.aviasales.ru/";
+            Configuration.pageLoadStrategy = "eager";
+            Configuration.remote = "https://user1:1234@" + System.getProperty("wdHost", "selenoid.autotests.cloud") + "/wd/hub";
+            Configuration.browser = System.getProperty("browser", "chrome");
+            Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+            Configuration.browserVersion = System.getProperty("browserVersion", "120.0");
+            Configuration.remote = System.getProperty("wdHost");
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        }
 
 
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "120.0");
-        Configuration.remote = System.getProperty("wdHost");
-        Configuration.baseUrl = "https://www.aviasales.ru/";
-        Configuration.timeout = 10000;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
 
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
 
-    @AfterEach
-    void addAttachment() {
-        Selenide.closeWebDriver();
-    }
-}
+        @AfterEach
+        void addAttachment () {
+            Attach.pageSource();
+            Attach.screenshotAs("Last screenshot");
+            Attach.addVideo();
+            if (!WebDriverRunner.isFirefox()) {
+                Attach.browserConsoleLogs();
+            } } }
+
