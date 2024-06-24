d6faff8c939c1a0d9c3f0af657862ecc8b8ef03d
Andrey <zizkun.av@list.ru>
2024-06-24 20:34:23 +0400

    Commit

--- src/test/java/tests/NowPageTests.java
+++ src/test/java/tests/NowPageTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
 import pages.NowPage;
-import pages.ProfilePage;
 
 @Epic("UI tests")
 @Owner("Andrey Zhizhkun")
@@ -14,9 +13,6 @@ import pages.ProfilePage;
 public class NowPageTests extends TestBase {
     public MainPage mainpage = new MainPage();
     public NowPage nowpage = new NowPage();
-
-    public ProfilePage settingspage = new ProfilePage();
-    public ProfilePage profilepage = new ProfilePage();
     @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Button name - 'Список мест (10)'")
     @Tag("all")
@@ -37,43 +33,5 @@ public class NowPageTests extends TestBase {
         mainpage.openPage()
                 .clickButtonNow();
         nowpage.clickButtonRusTravaling("Калининград");
-    }
-
-
-
-
-
-
-
-    @Severity(SeverityLevel.CRITICAL)
-    @DisplayName("Availability of text 'Доступ к поддержке и подпискам на цены' on the 'Your Profile' page")
-    @Tag("all")
-    @Test
-    void haveTextInProfileTest() {
-        mainpage.openPage()
-                .clickButtonProfile();
-        profilepage.checkTextAboutPrice();
-    }
-
-
-    @Severity(SeverityLevel.CRITICAL)
-    @DisplayName("Presence of text at the 'Subscribe to Aviasails newsletter' checkbox")
-    @Tag("all")
-    @Test
-    void haveSalesTicketseTest() {
-        mainpage.openPage()
-                .clickButtonProfile();
-        settingspage.checkTextElement("Подписаться на рассылку Авиасейлс");
-    }
-
-
-    @Severity(SeverityLevel.CRITICAL)
-    @DisplayName("Checking for incorrect text - 'Открыть Ostrovok.ru в новой вкладке'")
-    @Tag("all")
-    @Test
-    void haveSalesTicketsqTest() {
-        mainpage.openPage()
-                .clickButtonProfile();
-        profilepage.checkUnsuccesTextElement("Открыть Ostrovok.ru в новой вкладке");
     }
 }
--- src/test/java/tests/ProfilePageTests.java
+++ src/test/java/tests/ProfilePageTests.java
@@ -12,5 +12,39 @@ import pages.ProfilePage;
 @Feature("Checking the functionality of the profile page")
 public class ProfilePageTests extends TestBase {
 
+    public MainPage mainpage = new MainPage();
+    public ProfilePage settingspage = new ProfilePage();
+    public ProfilePage profilepage = new ProfilePage();
 
+
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Availability of text 'Доступ к поддержке и подпискам на цены' on the 'Your Profile' page")
+    @Tag("all")
+    @Test
+    void haveTextInProfileTest() {
+        mainpage.openPage()
+                .clickButtonProfile();
+        profilepage.checkTextAboutPrice();
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Presence of text at the 'Subscribe to Aviasails newsletter' checkbox")
+    @Tag("all")
+    @Test
+    void haveSalesTicketseTest() {
+        mainpage.openPage()
+                .clickButtonProfile();
+        settingspage.checkTextElement("Подписаться на рассылку Авиасейлс");
+    }
+
+
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Checking for incorrect text - 'Открыть Ostrovok.ru в новой вкладке'")
+    @Tag("all")
+    @Test
+    void haveSalesTicketsqTest() {
+        mainpage.openPage()
+                .clickButtonProfile();
+        profilepage.checkUnsuccesTextElement("Открыть Ostrovok.ru в новой вкладке");
+    }
 }
