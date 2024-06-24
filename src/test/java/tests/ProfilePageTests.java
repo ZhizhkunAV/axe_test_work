742e9ad70a5ff512e08596d6ccc093b9b56ae00a
Andrey <zizkun.av@list.ru>
2024-06-24 22:30:19 +0400

    Commit

--- src/test/java/tests/ProfilePageTests.java
+++ src/test/java/tests/ProfilePageTests.java
@@ -10,7 +10,7 @@ import pages.ProfilePage;
 @Epic("UI tests")
 @Owner("Andrey Zhizhkun")
 @Feature("Checking the functionality of the profile page")
-@DisplayName("Testing Profile page functionality")
+
 public class ProfilePageTests extends TestBase {
 
     public MainPage mainpage = new MainPage();
