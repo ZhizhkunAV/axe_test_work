a9dc41c3edc4765b508091306cfad66799cc6595
Andrey <zizkun.av@list.ru>
2024-06-24 11:30:26 +0400

    Commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,33 +11,40 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
 public class TestBase {
 
-        @BeforeAll
-        static void beforeAll () {
-            Configuration.baseUrl = "https://www.aviasales.ru/";
-            Configuration.pageLoadStrategy = "eager";
-            //Configuration.remote = "https://user1:1234@" + System.getProperty("wdHost", "selenoid.autotests.cloud") + "/wd/hub";
-            Configuration.browser = System.getProperty("browser", "chrome");
-            Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-            Configuration.browserVersion = System.getProperty("browserVersion", "120.0");
-            Configuration.remote = System.getProperty("wdHost");
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://www.aviasales.ru/";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "firefox");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browser_version", "122.0");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remote_url",
+                "selenoid.autotests.cloud") + "/wd/hub";
 
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
-            Configuration.browserCapabilities = capabilities;
-
-            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        }
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
 
     @AfterEach
-    void addAttachment() {
-        Selenide.closeWebDriver();
-
-
-    } }
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        if (!WebDriverRunner.isFirefox()) {
+            Attach.browserConsoleLogs();
+        }
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
 
